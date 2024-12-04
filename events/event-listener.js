aae2d5975cf36ab3c57ad230e4677ff3f669d730
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2024-12-04 17:56:39 +0530

    feat : added events example

--- events/event-listener.js
+++ events/event-listener.js
@@ -15,3 +15,10 @@ event.eventEmitter.emit("print", "My name is Deepanshu Mehra");
 
 logger.log("Printing Logs .....");
 logger.log("Printing logs for second time.....");
+
+event.eventEmitter.once("only-once", (data) => {
+  console.log(data);
+});
+
+// event.eventEmitter.emit("error", new Error("whoops!"));
+event.eventEmitter.emit("only-once", "I will print message only once.");
